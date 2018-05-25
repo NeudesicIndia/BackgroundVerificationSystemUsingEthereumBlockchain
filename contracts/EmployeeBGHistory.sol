85db8763d13a37a43e505a86cb01a021bf671d3b
Pushpendra Singh <Pushpendra.Singh@neudesic.com>
2018-05-26 03:38:37 +0530

    History support

--- contracts/EmployeeBGHistory.sol
+++ contracts/EmployeeBGHistory.sol
@@ -1,15 +1,19 @@
 pragma solidity ^0.4.23;
 pragma experimental ABIEncoderV2;
 
+import "./UniqueId.sol";
+
 contract EmployeeBGHistory {
     struct Employee {
         string name;
         uint age;
         string communicationAddress;
         EmployeeHistory[] history;
+        uint[] historyIds;
     }
 
     struct EmployeeHistory{
+        uint historyId;
         string orgName;
         string joiningDate;
         string exitDate;
@@ -20,7 +24,7 @@ contract EmployeeBGHistory {
 
     mapping(string => Employee) employees;
     string[] public employeeKeys;
-
+    
     event logError(string error);
 
     //empId will be the PAN number
@@ -42,8 +46,10 @@ contract EmployeeBGHistory {
     function getEmployeeKeys() view public returns (string[]) {
         return employeeKeys;
     }
-    function getEmployee(string empId) view public returns (string, uint, string) {
-        return (employees[empId].name, employees[empId].age, employees[empId].communicationAddress);
+    function getEmployee(string empId) view public returns (string, uint, string, uint[]) {
+        require(bytes(employees[empId].name).length != 0, "Employee does not exist on the chain. Use setEmployee to create a new employee.");
+        
+        return (employees[empId].name, employees[empId].age, employees[empId].communicationAddress, employees[empId].historyIds);
     }
     function getEmployeeCount() view public returns (uint) {
         return employeeKeys.length;
@@ -55,9 +61,20 @@ contract EmployeeBGHistory {
         require(bytes(joiningDate).length != 0 || bytes(exitDate).length != 0, "One of joiningDate or exitDate needs to be supplied.");
 
         var employee = employees[empId];
-
-        var history = EmployeeHistory(orgName, joiningDate, exitDate);
+        var historyId = UniqueId.getUniqueId();
+        var history = EmployeeHistory(historyId, orgName, joiningDate, exitDate);
+        employee.historyIds.push(historyId) - 1;
         employee.history.push(history) - 1;
     }
-
+    function getHistory(string empId, uint historyId) view public returns(string, string, string) {
+        var employee = employees[empId];
+        require(bytes(employee.name).length != 0, "Employee does not exist on the chain. Use setEmployee to create a new employee.");
+        for(uint i = 0; i < employee.history.length; i++){
+            var history = employee.history[i];
+            if(history.historyId == historyId)
+                return (history.orgName, history.joiningDate, history.exitDate);
+        }
+        return ("", "", "");
+    }
 }
+
--- contracts/UniqueId.sol
+++ contracts/UniqueId.sol
@@ -7,15 +7,14 @@ library UniqueId {
     *
     * Sample: 0xf4a8f74879182ff2a07468508bec89e1e7464027		          
     */  
-    function getUniqueId() public view returns (address) 
+    function getUniqueId() public view returns (uint) 
     {
-
         bytes20 b = bytes20(keccak256(msg.sender, now));
         uint addr = 0;
         for (uint index = b.length-1; index+1 > 0; index--) {
             addr += uint(b[index]) * ( 16 ** ((b.length - index - 1) * 2));
         }
-
-        return address(addr);
+        return addr;
+        //return address(addr);
     }
 }
