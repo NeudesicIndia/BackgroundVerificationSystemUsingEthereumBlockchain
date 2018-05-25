e90d6f003efeee60af1a9a7990064e566094f494
Pushpendra Singh <Pushpendra.Singh@neudesic.com>
2018-05-26 04:30:32 +0530

    link

--- migrations/2_contracts_deploy.js
+++ migrations/2_contracts_deploy.js
@@ -1,7 +1,10 @@
+var UniqueId = artifacts.require("./UniqueId.sol");
 var EmployeeBGHistory = artifacts.require("./EmployeeBGHistory.sol");
-//var Emp = artifacts.require("./Emp.sol");
 
 module.exports = function(deployer) {
+  deployer.deploy(UniqueId);
+  deployer.link(UniqueId, EmployeeBGHistory);
   deployer.deploy(EmployeeBGHistory);
-  //deployer.deploy(Emp);
 };
+
+
